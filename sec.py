498704439be8cc6aa16ad4637f947f1845074786
Sagar <sagarxyadav@gmail.com>
2023-02-09 13:25:01 -0500

    Update sec.py

--- sec.py
+++ sec.py
@@ -1,18 +1,20 @@
 # Importing required libraries
-from scapy.all import *
+import plotly.express as px
 import pandas as pd
+import subprocess
 
-# Reading the PCAP file
-packets = rdpcap('sample.pcap')
+# Capturing live packets using Tshark
+result = subprocess.Popen(['tshark', '-i', 'en0', '-T', 'fields', '-e', 'frame.time', '-e', 'ip.src', '-e', 'ip.dst', '-e', 'ip.proto', '-e', 'frame.len'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, errors = result.communicate()
 
-# Converting the packets to a pandas dataframe
-df = pd.DataFrame(columns=['Time', 'Source', 'Destination', 'Protocol', 'Length'])
-for packet in packets:
-    row = {'Time': packet.time, 'Source': packet[IP].src, 'Destination': packet[IP].dst, 'Protocol': packet[IP].proto, 'Length': len(packet)}
-    df = df.append(row, ignore_index=True)
+# Converting the output of Tshark to a pandas dataframe
+rows = output.splitlines()
+data = [row.decode().split('\t') for row in rows]
+df = pd.DataFrame(data, columns=['Time', 'Source', 'Destination', 'Protocol', 'Length'])
+df = df.astype({'Length': 'int', 'Protocol': 'int'})
 
-# Implementing your AI algorithms here to analyze the data for potential threats
-# ...
+# Creating a bar chart to show the distribution of traffic by protocol
+fig = px.bar(df, x='Protocol', y='Length', color='Protocol', title='Distribution of Traffic by Protocol')
 
-# Displaying the results
-print(df)
+# Showing the visualization
+fig.show()
